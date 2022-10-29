df51bec4f882927cd5643a79b2a31cc9d9d6b0af
Zach Tang <zachtangoj@gmail.com>
2022-10-29 12:26:11 -0500

    update hello to include Spring Summer and Fall

--- src/hello/Hello.jsx
+++ src/hello/Hello.jsx
@@ -8,7 +8,7 @@ function Hello(){
 
             <p>
                 I'm a junior studying Computer Science. I've recently finished a SWE internship at Capital One, where I was able to develop
-                an impactful and interesting project, and now I'm looking for Summer'23 internship opportunities where I can continue
+                an impactful and interesting project, and now I'm looking for Spring, Summer, and Fall'23 internship opportunities where I can continue
                 take responsibility and ownership of my work.
             </p>
 
